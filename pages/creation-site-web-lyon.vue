975fe1d655840ac7a83825d2b218e3bc63ec1c9b
MarjorieMartz <78167429+MarjorieMartz@users.noreply.github.com>
2023-08-23 14:31:14 +0000

    nv projet supetixs et asg

--- pages/creation-site-web-lyon.vue
+++ pages/creation-site-web-lyon.vue
@@ -2,36 +2,36 @@
     <main>
       <!-- <TheHero> A Nuxt Content v2 Blog with Bulma </TheHero> -->
       <NosservicesHeader 
-      text="Votre agence de création de site web à Lyon"
-      description=" Notre agence web située à Lyon s’occupe de la réalisation de site internet personnalisée pour représenter aux mieux votre entreprise. L’expertise de l’agence comprend la création de site web unique et professionnel qui répondra aux besoins spécifiques de vos utilisateurs. Du webdesign au développement, nous faisons de la conception de site vitrine et e-commerce." 
+      text="Votre agence de création de site vitrine à Lyon"
+      description="Notre agence web située à Lyon s’occupe de la conception ou refonte de site vitrine entièrement personnalisée afin de représenter au mieux l'image de votre entreprise. Cette expertise comprend le design de votre site, la création et le développement. Notre agence vous assure un site web unique et professionnel qui répondra à vos besoins et à ceux de vos utilisateurs. " 
        />
        
       <BackImage backImage ="/img/novagraphy/novagraphy3.png"/>
        <NosservicesReveal
        title_reveal="Création de site sur mesure"
-      reveal="L’agence web à Lyon qui vous propose des sites innovants et créatifs qui répondent aux besoins de vos utilisateurs. Nous répondons à vos problématiques en vous créant une vitrine virtuelle. Novadigital vous accompagne tout au long de la création de votre site web jusqu’au référencement SEO pour garantir une vie longue terme à votre site vitrine. Il est important d’alimenter régulièrement en termes de contenu votre site web pour le maintenir en forme et optimiser ses chances d’être visible." 
+      reveal="Notre agence web à Lyon vous propose des sites innovants et créatifs qui vous correspondent. Nous transposons votre société en une vitrine de marque en ligne. Novadigital vous accompagne tout au long de la création d'un site web jusqu’au référencement naturel SEO, pour garantir une vie longue terme à votre site vitrine. Il est important de l'alimenter régulièrement en terme de contenu pour le maintenir en forme et optimiser ses chances d’être visible." 
       />
       <BackImage backImage ="/img/studiodino/studiodino3.png"/>
       <NosservicesNotreplus
       title="Développement de votre site"
-      sous_title1="Création de site full code"
-      text1="L'utilisation de plates-formes de création de sites web ou de CMS peut souvent entraîner une surcharge de code et des temps de chargement plus lents. En écrivant tout le code du site web, il est possible d'optimiser chaque ligne de code pour des performances optimales, ce qui se traduit par un temps de chargement plus rapide et une expérience utilisateur plus fluide."
-      sous_title2="Un site web sans limite"
-      text2="En écrivant tout le code du site web, il n'y a pas de limite à la manière dont le site web peut être amélioré et mis à jour. Les développeurs peuvent facilement ajouter de nouvelles fonctionnalités et effectuer des mises à jour pour garantir que le site web reste à jour et pertinent."
-      sous_title3="Webdesign unique"
-      text3="Les plates-formes de création de sites web et les CMS offrent souvent des options limitées en termes de personnalisation. En écrivant tout le code du site web, il est possible de personnaliser chaque aspect du site web en fonction des besoins spécifiques de l'utilisateur. Cela permet de créer un site web unique et personnalisé."
+      sous_title1="La clé d'une site vitrine efficace"
+      text1="Créer son site vitrine peut être fastidieux. C'est pour cela que nous mettons en oeuvre nos compétences pour vous aiguiller et vous accompagner dans la mise en place de votre visibilité en ligne. Au delà de participer à la création votre site web, nous nous plaçons en tant que consultant pour vous orienter dans les choix techniques en fonction de vos stratégies. La communication entre vous et nous est impérative et c'est celle-ci, qui pourra nous faire avancer ensemble. "
+      sous_title2="Solution de site full code"
+      text2="Nous vous proposons le développement de chaque page en full-code. Ceci présente plusieurs avantages tel que la personnalisation intégrale de la prestation. Ce service vous permet de personnaliser tous les aspects de votre site de manière efficace. Toutes les fonctionnalités peuvent être envisager pour répondre à vos clients. Une source infini de technique peuvent être installé pour booster et donner une plus value à votre site."
+      sous_title3="Solution de site personnalisé avec Wordpress"
+      text3="Notre agence vous propose également de développer votre site avec un CMS tel que Wordpress. En alliant CMS et code, la compatibilité des deux peut s'avérer très compétente en terme d'optimisation. En ajoutant du code dans Wordpress, il permet d'ajouter une grande personnalisation tout en gardant des interfaces graphiques faciles à utiliser. "
       />
        <NosservicesHorizontal 
        title1="Définir vos objectifs"
-       text1="Avec un membre de l’agence digitale, nous définissons ensemble vos besoins et attentes afin d'en conclure des objectifs communs. Cette phase d'introduction nous est essentielle pour vous proposer le développement qui vous correspond au mieux. Nous sommes ouverts à toutes vos interrogations concernant la création de votre site web."
+       text1="Avec un membre de l’agence digitale, nous définissons ensemble vos besoins et attentes afin d'en conclure des objectifs communs. Cette phase d'introduction nous est essentielle pour vous proposer le développement qui vous correspond au mieux. La communication est la clé de la réussite d'un projet. Pendant tout le long du projet, vous pouvez nous faire part d'informations ou changements autours de votre activité."
        title2="Création et webdesign du site web"
-       text2="Chaque projet est unique. C'est pour cela que notre agence web effectue des recherches afin de bien comprendre votre future cible. Par la suite, nous vous présenterons un webdesign sur mesure, adapté au brief vu en amont. Ce prototype, personnalisé de votre futur site internet, est la première ébauche du projet."
+       text2="Chaque projet est unique. C'est pour cela que notre agence web effectue des recherche afin de bien comprendre votre future cible et ainsi vous présenter une solution de qualité. Par la suite, nous vous présenterons un webdesign sur mesure, adapté à nos recherche et briefs communs. Ce prototype est la première ébauche du projet. Cet outil va nous permettre à échanger autour du futur site web jusqu'à atteindre une prototype optimisé."
        title3="Développement du site internet"
-       text3="L’intégration web, l’une des grandes étapes de votre projet. Cela consiste à développer votre site internet, gérer votre nom de domaine, puis mettre votre site en ligne afin qu'il soit utilisable. Vous pouvez maintenant accéder à votre site web sur les moteurs de recherche."
-       title4="Accompagnement référencement SEO"
-       text4="Nous développons votre site web en tenant compte du référencement. Pour des résultats concrets, nous vous accompagnons sur la durée, notamment pour vous permettre de vous positionner en tête des résultats de recherche pour les termes les plus pertinents. Le référencement naturel (SEO) peut se traduire par une augmentation du trafic qualifié sur votre site web, donc de nouvelles opportunités commerciales."
+       text3="L’intégration web est l’une des plus grandes étapes du projet. Elle consiste à développer votre site internet, grâce à l'utilisation d'une CMS ou en full-code. Nos développeurs experts se chargeront de coder votre site et de le mettre en ligne afin qu'il soit utilisable par tous les internautes. Si besoin, nous pouvons nous occuper de la gestion de l'hébergement de site."
+       title4="Accompagnement référencement naturel SEO"
+       text4="Nous développons votre site web en tenant compte du référencement. Pour des résultats concrets, nous vous accompagnons sur la durée, notamment pour vous permettre de vous positionner en tête des résultats de moteur recherche. Pour assurer une bonne performance, il est préférable de commencer l'optimisation Google dès la conception digitale de votre site internet. Cette présence et visibilité en ligne se traduire par une augmentation du trafic qualifié sur votre site web, donc de nouvelles opportunités commerciales."
        title5="Hébergement et maintenance du site web"
-       text5="Créer un site internet inclut la question de l’hébergement. Nous sommes évidemment présentes pour vous conseiller. Choisir un hébergeur adéquat est impératif pour une bonne expérience sur votre site. Les équipes de l’agence Novadigital s’occupent également des mises à jour récurrentes et assurent la protection et la sécurité de votre site internet."
+       text5="Créer un site internet inclut la question de l’hébergement. Nous sommes évidemment présent pour vous conseiller dans ce choix. Choisir un hébergeur adéquat est impératif pour une bonne expérience sur votre site. Un hébergeur bien optimisé garantie une navigation fluide en matière d'expérience et permet à ce que le site n'est d'accident à cause d'un éventuel surplus de trafic. Notre agence Novadigital s’occupent de la maintenance de votre site, des mises à jour récurrentes et assurent la protection et la sécurité de votre site internet."
        />
        <PresentationProjet/>
        <BlogArticleDroite/>
@@ -46,7 +46,7 @@ definePageMeta({
 useHead({
   title: 'Votre agence de création de site web à Lyon - Novadigital',
   meta: [
-    { name: 'description', content: "Novadigital, agence digitale spécialisée pour création de site vitrine, site e-commerce et d’application mobile situé à Lyon et alentours " }
+    { name: 'description', content: "Novadigital, agence digitale spécialisée dans la création de site vitrine, site e-commerce et d’application mobile situé à Lyon et alentours " }
   ],
 })
 
---
+++ pages/projet/asg.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+      <ProjetHero
+         text="ASG"
+         image ="/img/ASG/asg-1.png"
+      />
+      <ProjetPresentation
+      projetReveal="Nous avons eu l’opportunité de travailler sur un projet captivant en développement le site ASG. Notre rôle principal a été axé sur le développement technique et la création d'une expérience en ligne optimale en utilisant les technologies modernes. "
+      />
+      <a href="https://www.alexander-strategy.com/">
+        <Button class="bouton_projet" text="Site web" />
+    </a>
+      <ProjetDeuxieme
+      image ="/img/ASG/asg-2.png"
+      title ="Développement web dynamique"
+      p1="Nous avons pris en charge le développement complet du site web “Alexander strategy group” en utilisant les technologies suivantes : Vue.js 3 et Nuxt.js 3. Ce sont des technologies reconnues pour leur capacité à créer des interfaces utilisateur réactives et performantes. Cela est très utilisé pour réaliser des sites internet dynamiques comme peu l’être ASG."
+      p2="Grâce à son approche basée sur les composants, j'ai pu concevoir une architecture modulaire qui permet une maintenance simplifiée et une évolutivité aisée."
+  
+      />
+      <ProjetDeuxieme
+      image ="/img/ASG/ASG_3.png"
+      title="Mis en place d’un formulaire"
+      p1="Pour répondre aux besoins d'interaction avec les visiteurs du site, nous avons mis en œuvre une fonctionnalité spécifique. Nous avons créé un formulaire intuitif et convivial. Les utilisateurs peuvent désormais interagir avec ASG en soumettant leurs demandes et leurs informations directement via le formulaire en ligne, créant ainsi une communication fluide entre les parties."
+     />
+      <ProjetDeuxieme
+      image ="/img/ASG/asg-4.png"
+      title=""
+      p1=""
+  
+      />
+  
+    </div>
+</template>
+  
+  <script setup>
+  definePageMeta({
+    layout: "custom",
+  });
+  useHead({
+  title: "Développement du site vitrine Alexander strategy group",
+  meta: [
+    { name: 'description', content: "Présentation du projet Alexander strategy group. Projet dont l'on était en charge du développement du site vitrine." }
+  ],
+  })
+  </script>
+  
+  <style lang="scss" scoped>
+  
+  </style>
